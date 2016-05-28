976856d2491cf671528bec22e48277c647dc6b20
zegelink <supercomboooo@gmail.com>
2016-05-27 21:38:31 -0700

    fix php header

--- index.php
+++ index.php
@@ -1,29 +1,27 @@
 <?php
     session_start();
     ?>
-
+<?php
+    if(!empty($_SESSION["username"])) {
+        
+        
+        echo " Welcome back! ";
+        echo ($_SESSION["username"]);
+        echo '<br>';
+        echo '<a href="enterName.php">  Enter Password</a>';
+        echo '<br>';
+        echo '<div><a href="checkExistingPassword.php">Check Password  </a> </div>';
+        echo '<div><a href="logout.php"> Logout</a></div>';
+    } else {
+        header("Location: login.html");
+        echo 'You are not logined, click <a href="login.html">here</a> to login.';
+    }
+    ?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Passbook</title>
 </head>
 <body>
-
-<?php
-    if(!empty($_SESSION["username"])) {
-        echo " Welcome back!";
-        echo ($_SESSION["username"]);
-        echo '<br>';
-
-        echo '<a href="enterName.php">  Enter Password</a>';
-        echo '<br>';
-        echo '<div><a href="checkExistingPassword.php">Check Password  </a> </div>';
-        echo '<div><a href="logout.php"> Logout</a></div>';
-
-    } else {
-        echo 'You are not logined, click <a href="login.html">here</a> to login.';
-        header("Location: login.html");
-    }
-    ?>
 </body>
 </html>
--- logout.php
+++ logout.php
@@ -1,20 +1,17 @@
 <?php
     session_start();
     ?>
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
     // remove all session variables
     session_unset();
-    
     // destroy the session
     session_destroy();
+    header( "refresh:3; url=index.php" );
     echo "You have been logout successfully";
-    header( "refresh:3;url=index.php" );
     echo 'You\'ll be redirected in about 3 secs. If not, click <a href="index.php">here</a>.';
     ?>
-
+<!DOCTYPE html>
+<html>
+<body>
 </body>
 </html>
