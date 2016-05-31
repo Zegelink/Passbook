cbff7d2ef309e12cc98a2d7966168aceb08e77f2
zegelink <supercomboooo@gmail.com>
2016-05-30 22:21:38 -0700

    Randomly generate password from the style users choose working

--- enterPassword.php
+++ enterPassword.php
@@ -2,11 +2,157 @@
     session_start();
     $wid = $_GET['wid'];
     $uid = $_SESSION['uid'];
+    $styleSelected = $_POST['styleSelected'];
+    echo $styleSelected;
     
     include 'connect.php';
     //make sure the user logined has the permission to this wid
     include 'widVerify.php';
-    $sql = "SELECT * FROM `cs340_chencho`.`key_pb` WHERE wid = $wid";
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="../../favicon.ico">
+
+<title>Check existing password</title>
+
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+<!-- Custom styles for this template -->
+<link href="style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<script>
+function genPass(){
+    return;
+}
+
+function randomString(length) {
+    return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+}
+</script>
+
+<div class="container">
+
+<form action="enterPassword.php?wid=<?php echo $wid ?>" class="form-signin" method="post" role="form">
+<h2 class="form-signin-heading" align="center"><b>Enter New Account: </b></h2>
+<br>
+<label for="name">Account</label>
+<input type="text" id="account" name="account"class="form-control" placeholder="" required autofocus>
+
+<label for="sid">Password</label>
+<?php
+    $passGenerated;
+    //if user wants to generated a random password
+    if (!empty($styleSelected)){
+        $sqlStyle = "SELECT * FROM `cs340_chencho`.`itemLibrary_pb` WHERE sid = $styleSelected";
+        $resultStyle = $conn->query($sqlStyle);
+        if ($resultStyle->num_rows > 0 ){
+            $counter1 = 0;
+            $itemArray;
+            while ($row = $resultStyle->fetch_assoc()){
+                $itemArray[$counter1] = $row["item"];
+                $counter1++;
+            }
+        }
+        for ($i = 0; $i <3; $i++){
+            $number = mt_rand(0, $counter1-1);
+            $passGenerated = $passGenerated. $itemArray[$number];
+            $number = mt_rand(0,99);
+            $passGenerated = $passGenerated. $number;
+        }
+        $number = mt_rand(0,4);
+        $specialChar = array('$','%','&','*','#');
+        $passGenerated = $passGenerated. $specialChar[$number];
+    }
+    echo '<input type="text" id="password" name="password"class="form-control" placeholder="" value = "'. $passGenerated. '"  required >';
+?>
+    
+<label for="sid">Comment</label>
+<input type="text" id="comment" name="comment"class="form-control" placeholder="Optional"  >
+<br>
+
+
+
+<button class="btn btn-lg btn-success btn-block" type="submit">Confirm</button>
+<a href="index.php" class="btn btn-lg btn-danger btn-block" type="submit">Cancel</a> <br>
+</form>
+
+</div> <!-- /container -->
+
+<?php
+    $sqlStyle = "SELECT * FROM `cs340_chencho`.`style_pb` WHERE uid = $uid OR uid = -1";
+    $resultStyle = $conn->query($sqlStyle);
+    if ($resultStyle->num_rows > 0 ){
+        $counter1 = 0;
+        $sidArray;
+        while ($row = $resultStyle->fetch_assoc()){
+            $styleArray[$counter1] = $row["style"];
+            $sidArray[$counter1] = $row["sid"];
+            $counter1++;
+        }
+        echo '<form action ="enterPassword.php?wid='. $wid. '" class="form-signin" method="post" role="form">';
+        echo '<select name="styleSelected" onchange="randomString(5)" required>';
+    for ($i =  (count($styleArray) - 1); $i >= 0; $i-- ){
+        $value = $sidArray[$i];
+        echo '<option value="'. $value. '">'. $styleArray[$i]. '</option>';
+    }
+    echo '</select>';
+    echo '<input type = "submit" value="GeneratePassword">';
+    echo '</form>';
+    }
+    ?>
+
+<?php
+// If the values are posted, insert them into the database.
+    $account = $_POST["account"];
+    $password = $_POST["password"];
+
+    if (!empty($account) && !empty($password)) {
+        $comment = $_POST["comment"];
+        
+        $result =0;
+        
+        if(!empty($account) && !empty($password)){
+                $query = "INSERT INTO `cs340_chencho`.`key_pb` (wid, account, passkey, comment) VALUES ('$wid', '$account', '$password', '$comment' )";
+                $result = mysqli_query($conn,$query);
+            }
+            
+            if($result == 1){
+                $msg = "<p align=center>Account entered.</p>";
+                echo $msg;
+                //echo '<p align = center>Refresh the page <a href = "enterPassword.php?wid=' .$wid. '">here </a></p>';
+            }
+            else {
+                echo "Failed to insert";
+            }
+        
+
+
+
+        //find the student ID in the database
+        //if the number of ID is not equal to 1
+        
+        //find the student ID in the database
+        //if the number of ID is not equal to 1
+        
+
+    }
+?>
+<br>
+
+<?php    $sql = "SELECT * FROM `cs340_chencho`.`key_pb` WHERE wid = $wid";
     $result = $conn->query($sql);
     
     if ($result->num_rows > 0 ){
@@ -14,8 +160,6 @@
         $accountArray;
         $passkeyArray;
         $commentArray;
-        
-        
         while($row = $result->fetch_assoc()){
             
             $accountArray[$counter] = $row["account"];
@@ -43,98 +187,8 @@
     else{
         echo "<p align=center>You don't have any account info yet</p>";
     }
-    
-    
-
-
-    ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-<meta name="description" content="">
-<meta name="author" content="">
-<link rel="icon" href="../../favicon.ico">
-
-<title>Check existing password</title>
-
-<!-- Bootstrap core CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
-<!-- Custom styles for this template -->
-<link href="style.css" rel="stylesheet">
-
-</head>
-
-<body>
-
-<div class="container">
-
-<form action="enterPassword.php?wid=<?php echo $wid ?>" class="form-signin" method="post" role="form">
-<h2 class="form-signin-heading" align="center"><b>Enter New Account: </b></h2>
-<br>
-<label for="name">Account</label>
-<input type="text" id="account" name="account"class="form-control" placeholder="" required autofocus>
-
-<label for="sid">Password</label>
-<input type="text" id="password" name="password"class="form-control" placeholder="" required >
-
-<label for="sid">Comment</label>
-<input type="text" id="comment" name="comment"class="form-control" placeholder="Optional"  >
-<br>
-
-
-
-<button class="btn btn-lg btn-success btn-block" type="submit">Confirm</button>
-<a href="index.php" class="btn btn-lg btn-danger btn-block" type="submit">Cancel</a> <br>
-</form>
-
-</div> <!-- /container -->
-
-
-<?php
-    
-    
-// If the values are posted, insert them into the database.
-    $account = $_POST["account"];
-    $password = $_POST["password"];
-
-    if (!empty($account) && !empty($password)) {
-        $comment = $_POST["comment"];
-        
-        $result =0;
-        
-        if(!empty($account) && !empty($password)){
-                $query = "INSERT INTO `cs340_chencho`.`key_pb` (wid, account, passkey, comment) VALUES ('$wid', '$account', '$password', '$comment' )";
-                $result = mysqli_query($conn,$query);
-            }
-            
-            if($result == 1){
-                $msg = "<p align=center>Account entered.</p>";
-                echo $msg;
-                echo '<p align = center>Refresh the page <a href = "enterPassword.php?wid=' .$wid. '">here </a></p>';
-            }
-            else {
-                echo "Failed to insert";
-            }
-        
-
-
-
-        //find the student ID in the database
-        //if the number of ID is not equal to 1
-        
-        //find the student ID in the database
-        //if the number of ID is not equal to 1
-        
-
-    }
-mysqli_close($conn);
+    mysqli_close($conn);
 ?>
-<br>
+    
 </body>
 </html>
