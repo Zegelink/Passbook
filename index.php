1ca1d987fb3c70a9c9ab7ac813597c5475a55454
zegelink <supercomboooo@gmail.com>
2016-05-31 22:19:50 -0700

    update navBar, to be continued

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Passbook</title>
+
+<!-- Bootstrap  CSS and FontAwesome too -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+<!-- Custom styles for this template -->
+<link href="navbar.css" rel="stylesheet">
+
+</head>
+
+<script src="http://www.w3schools.com/lib/w3data.js"></script>
+<body>
+<div w3-include-html="navBar.html"></div>
+<script>
+w3IncludeHTML();
+</script>
+</body>
+
+</html>
 <?php
     session_start();
     include 'connect.php';
@@ -55,11 +78,4 @@
         echo 'You are not logined, click <a href="login.html">here</a> to login.';
     }
     ?>
-<!DOCTYPE html>
-<html>
-<head>
-<title>Passbook</title>
-</head>
-<body>
-</body>
-</html>
+
